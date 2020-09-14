f1c638edd5089d3253694fc633c318e3eb0b0449
Dean Hadzi <deanhadzi82@gmail.com>
2020-09-13 21:24:01 -0400

    oop1 done.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle():
+    """Base Class. The one to rule them all."""
+    pass
+
+class GroundVehicle(Vehicle):
+    """Left Branch, middle level class."""
+    pass
+
+class Car(GroundVehicle):
+    """Left Branch, bottom level class."""
+    pass
+
+class Motorcycle(GroundVehicle):
+    """Left Branch, bottom level class."""
+    pass
+
+class FlightVehicle(Vehicle):
+    """Right Branch, middle level class."""
+    pass
+
+class Starship(FlightVehicle):
+    """Right Branch, bottom level class."""
+    pass
+
+class Airplane(FlightVehicle):
+    """Right Branch, bottom level class."""
+    pass
